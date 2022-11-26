30cbedfc8754d18bbd2ecbbe5a04c621bff7307f
guerrerody <dorielizguerrero2000@gmail.com>
2022-11-26 15:39:49 -0400

    Nueva versión de SQL.

--- sql/db-vigilancia_1.0.sql
+++ sql/db-vigilancia_1.0.sql
@@ -32,9 +32,26 @@ CREATE TABLE cliente (
     telf_contac VARCHAR(12),
     status INTEGER NOT NULL DEFAULT 0,
 	CONSTRAINT pk_cliente PRIMARY KEY (id),
-	CONSTRAINT fk_cliente_tipo_cliente FOREIGN KEY (tipo_cliente_id) REFERENCES tipo_cliente(id),
-	CONSTRAINT uk_cedula UNIQUE (cedula)
+	CONSTRAINT fk_cliente_tipo_cliente FOREIGN KEY (tipo_cliente_id) REFERENCES tipo_cliente(id)
 );
+
+CREATE SEQUENCE IF NOT EXISTS tipo_alquiler_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+CREATE TABLE tipo_alquiler (
+    id INTEGER NOT NULL DEFAULT nextval('tipo_alquiler_id_seq'::regclass),
+    nombre VARCHAR(100) NOT NULL,
+    costo_uso NUMERIC(10,2), -- Costo de Uso Mensual
+    costo_mant NUMERIC(10,2), -- Costo de Mantenimiento Mensual
+	CONSTRAINT pk_tipo_alquiler PRIMARY KEY (id)
+);
+
+INSERT INTO tipo_alquiler (nombre) VALUES ('Bicicleta');
+INSERT INTO tipo_alquiler (nombre) VALUES ('Radio');
 
 CREATE SEQUENCE IF NOT EXISTS servicio_id_seq
     INCREMENT 1
@@ -46,35 +63,21 @@ CREATE SEQUENCE IF NOT EXISTS servicio_id_seq
 CREATE TABLE servicio (
     id INTEGER NOT NULL DEFAULT nextval('servicio_id_seq'::regclass),
     client_id INTEGER NOT NULL,
-    fecha DATE,
+    fec_in DATE NOT NULL,
+	fec_fin DATE NOT NULL,
     descr VARCHAR(150),
-    turno TIMESTAMP WITHOUT TIME ZONE,
-    vig_hab INTEGER,
-    status INTEGER,
+	status INTEGER NOT NULL DEFAULT 0,
     CONSTRAINT pk_servicio PRIMARY KEY (id),
-	CONSTRAINT fk_servicio_id_client FOREIGN KEY (client_id) REFERENCES cliente(id)
+	CONSTRAINT fk_servicio_cliente_id FOREIGN KEY (client_id) REFERENCES cliente(id)
 );
 
-CREATE SEQUENCE IF NOT EXISTS alquiler_id_seq
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    CACHE 1;
-
-CREATE TABLE alquiler (
-    id INTEGER NOT NULL DEFAULT nextval('alquiler_id_seq'::regclass),
-    id_serv INTEGER,
-    fechain DATE,
-    fechafin DATE,
-    descr VARCHAR(255),
-    cant INTEGER,
-    hruso INTEGER,
-    costou NUMERIC(10,2),
-    costomant NUMERIC(10,2),
-    status INTEGER,
-    CONSTRAINT pk_alquiler PRIMARY KEY (id),
-    CONSTRAINT fk_servicio_cod FOREIGN KEY (id_serv) REFERENCES servicio(id)
+CREATE TABLE servicio_extra (
+    servicio_id INTEGER NOT NULL,
+    tipo_alquiler_id INTEGER NOT NULL,
+    cant INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT pk_servicio_extra PRIMARY KEY (servicio_id, tipo_alquiler_id),
+	CONSTRAINT fk_servicio_id FOREIGN KEY (servicio_id) REFERENCES servicio(id),
+    CONSTRAINT fk_tipo_alquiler_id FOREIGN KEY (tipo_alquiler_id) REFERENCES tipo_alquiler(id)
 );
 
 CREATE SEQUENCE IF NOT EXISTS vigilante_id_seq
@@ -94,7 +97,8 @@ CREATE TABLE vigilante (
     telf VARCHAR(12),
     fec_ing DATE NOT NULL DEFAULT CURRENT_DATE,
     status INTEGER NOT NULL DEFAULT 0,
-    CONSTRAINT pk_vigilante PRIMARY KEY (id)
+    CONSTRAINT pk_vigilante PRIMARY KEY (id),
+	CONSTRAINT uk_cedula UNIQUE (cedula)
 );
 
 CREATE SEQUENCE IF NOT EXISTS horario_id_seq
@@ -106,15 +110,29 @@ CREATE SEQUENCE IF NOT EXISTS horario_id_seq
 
 CREATE TABLE horario (
 	id INTEGER NOT NULL DEFAULT nextval('horario_id_seq'::regclass),
-    id_vg INTEGER NOT NULL,
-    fechain DATE,
-    fechafin DATE,
-    horain TIME WITHOUT TIME ZONE,
-    horafin TIME WITHOUT TIME ZONE,
-    faltas INTEGER,
-    just INTEGER,
+    vigilante_id INTEGER NOT NULL,
+    fec_in DATE,
+    fec_fin DATE,
     CONSTRAINT pk_horario PRIMARY KEY (id),
-    CONSTRAINT fk_horario_id_vg FOREIGN KEY (id_vg) REFERENCES vigilante(id)
+    CONSTRAINT fk_horario_vigilante_id FOREIGN KEY (vigilante_id) REFERENCES vigilante(id)
+);
+
+CREATE SEQUENCE IF NOT EXISTS turno_id_seq
+   	INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+CREATE TABLE turno (
+    id INTEGER NOT NULL DEFAULT nextval('turno_id_seq'::regclass),
+    id_horario INTEGER NOT NULL,
+    fec_in TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    fec_fin TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    falta BOOLEAN NOT NULL DEFAULT FALSE,
+    just TEXT,
+    CONSTRAINT pk_turno PRIMARY KEY (id),
+    CONSTRAINT fk_turno_id_h FOREIGN KEY (id_horario) REFERENCES horario(id)
 );
 
 CREATE SEQUENCE IF NOT EXISTS nomina_id_seq
@@ -126,7 +144,7 @@ CREATE SEQUENCE IF NOT EXISTS nomina_id_seq
 
 CREATE TABLE nomina (
     id INTEGER NOT NULL DEFAULT nextval('nomina_id_seq'::regclass),
-    id_vg INTEGER NOT NULL,
+    vigilante_id INTEGER NOT NULL,
     fecha DATE,
     descr VARCHAR(255),
     diasextra INTEGER,
@@ -136,16 +154,18 @@ CREATE TABLE nomina (
     pagoextra NUMERIC(10,2),
     deduccion NUMERIC(10,2),
     CONSTRAINT pk_nomina PRIMARY KEY (id),
-    CONSTRAINT fk_nomina_id_vg FOREIGN KEY (id_vg) REFERENCES vigilante(id)
+    CONSTRAINT fk_nomina_vigilante_id FOREIGN KEY (vigilante_id) REFERENCES vigilante(id)
 );
 
 CREATE TABLE detalle_servicio (
-    id_vg INTEGER NOT NULL,
-    serv_cod INTEGER NOT NULL,
+    vigilante_id INTEGER NOT NULL,
+    servicio_id INTEGER NOT NULL,
+	turno_id INTEGER NOT NULL,
     resumen TEXT,
-    CONSTRAINT pk_detalle_servicio PRIMARY KEY (id_vg, serv_cod),
-    CONSTRAINT fk_detalle_cod_serv FOREIGN KEY (serv_cod) REFERENCES servicio(id),
-	CONSTRAINT fk_detalle_id_vg FOREIGN KEY (id_vg) REFERENCES vigilante(id)    
+    CONSTRAINT pk_detalle_servicio PRIMARY KEY (vigilante_id, servicio_id, turno_id),
+    CONSTRAINT fk_detalle_servicio_id FOREIGN KEY (servicio_id) REFERENCES servicio(id),
+	CONSTRAINT fk_detalle_vigilante_id FOREIGN KEY (vigilante_id) REFERENCES vigilante(id),
+	CONSTRAINT fk_detalle_id_t FOREIGN KEY (turno_id) REFERENCES turno(id)
 );
 
 CREATE SEQUENCE IF NOT EXISTS factura_id_seq
@@ -157,9 +177,7 @@ CREATE SEQUENCE IF NOT EXISTS factura_id_seq
 
 CREATE TABLE factura (
     id INTEGER NOT NULL DEFAULT nextval('factura_id_seq'::regclass),
-    id_client INTEGER,
-    cod_serv INTEGER,
-    cod_alq INTEGER,
+    servicio_id INTEGER,
     fecha_pago DATE,
     tipo_pago INTEGER,
     descr VARCHAR(255),
@@ -168,7 +186,5 @@ CREATE TABLE factura (
     total NUMERIC(10,2),
     status INTEGER,
 	CONSTRAINT pk_factura PRIMARY KEY (id),
-	CONSTRAINT fk_cod_serv FOREIGN KEY (cod_serv) REFERENCES servicio(id),
-	CONSTRAINT fk_factura_cod_alquiler FOREIGN KEY (cod_alq) REFERENCES alquiler(id),
-	CONSTRAINT fk_factura_id_client FOREIGN KEY (id_client) REFERENCES cliente(id)    
+	CONSTRAINT fk_servicio_id FOREIGN KEY (servicio_id) REFERENCES servicio(id) 
 );
